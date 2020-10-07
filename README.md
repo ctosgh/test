b118c7877660a778c8b1ea3aeee32ba981208b48
jsun <jacky.sun@watchguard.com>
2020-10-07 09:41:53 -0700

    Delete all old files

--- README.md
+++
@@ -1,4 +0,0 @@
-# test
-Add one line
-Add one line again
-This is test program for studying github
--- test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("this is my first test C program\n");
-    return 0;
-}
